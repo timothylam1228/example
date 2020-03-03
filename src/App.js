46e9ad92916c91672a0501982020528dbae02184
BuildTools <unconfigured@null.spigotmc.org>
2020-03-03 23:24:31 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+let styles = {
+    margin: 'auto',
+    width: '500px'
+  };
+  
+function App() {
+  return (
+	<div style={styles}>
+		<Carousel>
+			<div>
+				<img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/jrfyzvgzvhs1iylduuhj.jpg" alt="Hong Kong" />
+				<p className="legend">Hong Kong</p>
+			</div>
+			<div>
+				<img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/c1cklkyp6ms02tougufx.webp" alt="Singapore"/>
+				<p className="legend"></p>
+			</div>
+			<div>
+				<img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/e8fnw35p6zgusq218foj.webp" alt="Japan"/>
+				<p className="legend">Japan</p>
+			</div>
+			<div>
+				<img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp" alt="Las Vegas"/>
+				<p className="legend">Las Vegas</p>
+			</div>
+		</Carousel>
+	</div>
+  );
+}
+
+export default App;
